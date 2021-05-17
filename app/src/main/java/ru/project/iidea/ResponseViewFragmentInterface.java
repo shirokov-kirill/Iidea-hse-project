732111b47a5f853a5a9d296ce0851ad5a0029d39
shirokov-kirill <k.s.shirokov@mail.ru>
2021-05-17 17:48:29 +0300

    Add DB connection on MyResponses page

--- app/src/main/java/ru/project/iidea/ResponseViewFragmentInterface.java
+++ app/src/main/java/ru/project/iidea/ResponseViewFragmentInterface.java
@@ -3,4 +3,5 @@ package ru.project.iidea;
 public interface ResponseViewFragmentInterface {
     public void onBackButtonPressed();
     public void onUserIdClicked(long userID);
+    public void onRejectResponseButtonClicked(long responseID);
 }
--- app/src/main/java/ru/project/iidea/ResponsesFragmentInterface.java
+++ app/src/main/java/ru/project/iidea/ResponsesFragmentInterface.java
@@ -2,4 +2,5 @@ package ru.project.iidea;
 
 public interface ResponsesFragmentInterface {
     public void openCurrentResponse(Response response);
+    public void showToast(String message);
 }
